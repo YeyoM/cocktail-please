47da923f2fd3166120859f2b657839fdc1b3c7a0
YeyoM <yeyomoreno2003@hotmail.com>
2022-08-09 13:16:30 -0500

    adding images

--- components/cocktailCard/cocktailCard.js
+++ components/cocktailCard/cocktailCard.js
@@ -1,7 +1,11 @@
+import Image from "next/image"
+
 export default function cocktailCard() {
   return (
-    <div className="h-full w-4/5 mt-8 bg-violet-300 p-5 rounded-xl bg-opacity-60 backdrop-filter backdrop-blur-lg">
-      <p>hola</p>
+    <div className="h-full w-4/5 mt-8 bg-violet-300 p-5 rounded-xl bg-opacity-60 backdrop-filter backdrop-blur-lg flex flex-col items-center">
+      <div className="w-2/3 ">
+        <Image src="/cocktail.jpg" alt="cocktail" width={100} height={100} layout="responsive" />
+      </div>
     </div>
   )
 }
