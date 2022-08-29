8455aeefc72d4c88ba6c3e9eeae5e4fcc5ecea75
YeyoM <yeyomoreno2003@hotmail.com>
2022-08-29 18:12:31 -0500

    icon

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+
+class myDocument extends Document {
+  render () {
+    return <Html lang='en'>
+      <Head>
+        <link rel="icon" href="/icon.svg" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+        <meta name="theme-color" content="#5a1d85" />
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  }
+}
+
+export default myDocument
